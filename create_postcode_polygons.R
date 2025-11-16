5016601ca172abc5c0c77d87ac53a9ad102942a8
BlaiseKelly <blaise@kello.co.uk>
2025-11-16 21:36:33 +0100

    first commit

---
+++ create_postcode_polygons.R
@@ -0,0 +1,53 @@
+library(sf)
+library(mapview)
+library(raster)
+library(tmap)
+library(dplyr)
+
+## import UK polygons
+GB_counties <- st_read("dat/bdline_gpkg_gb/Data/bdline_gb.gpkg") |> 
+  mutate(area = as.numeric(st_area(geometry))) |> 
+  arrange(area)
+
+GB_all <- st_union(GB_counties)
+
+GB_postcodes <- st_read("dat/codepo_gpkg_gb/Data/codepo_gb.gpkg")
+
+# pick a county
+cty <- GB_counties[1,]
+
+# pick out the postcodes in the county
+cty_pc <- GB_postcodes[cty,]
+
+## make all points as one
+d <- st_union(cty_pc)
+## create voroni
+v <- st_voronoi(d)
+
+## create voroni polygons
+p1 <- st_as_sf(st_intersection(st_cast(v), cty), crs = 27700)
+
+## define the geometry as polygons
+pc_vor <- st_cast(p1, "MULTIPOLYGON") |> 
+  st_join(cty_pc)
+
+# get basemap
+bg <- basemaps::basemap_raster(cty, map_res = 0.99, map_service = "carto", map_type = "light")
+bg_m <- mask(bg, st_transform(cty,3857))
+
+tm2 <- tm_shape(bg_m)+
+  tm_rgb()+
+  tm_shape(cty) +
+  tm_polygons(lwd = 3, fill_alpha = 0)+
+  tm_shape(pc_vor)+
+  tm_polygons(fill_alpha = 0)+
+  tm_shape(cty_pc)+
+  tm_dots(size = 0.1)+
+  tm_layout(frame = FALSE)+
+  tm_title(text = paste0(cty$Name, "\npostcode centroids\nand generated Voroni cells"),position = c(0.01,0.96),size = 1)
+
+# save plot
+tmap_save(tm2, "voroni.png")
+
+# write them out
+write_sf(pc_vor, "voronis.gpkg")
